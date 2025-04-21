8e71799f3215ed57ffa16cd4e99d95a726fc2b1d
naveenpandey <naveenppandey8080@gmail.com>
2025-04-21 21:06:02 +0530

    IsPalindrome or not using two pointer concept

---
+++ src/Problem/IsPalindrome.java
@@ -0,0 +1,22 @@
+package Problem;
+
+public class IsPalindrome {
+    public static void main(String[] args) {
+
+        String str = "naman";
+        System.out.println(isPalindrome(str));
+    }
+
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length()-1;
+        while(left < right) {
+            if(str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
