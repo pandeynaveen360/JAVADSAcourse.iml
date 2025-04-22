65ee9452a9a8502b13f7c63b3af9b71f40dd5885
naveenpandey <naveenppandey8080@gmail.com>
2025-04-22 21:38:47 +0530

    replaceAscii.java

---
+++ src/Array_Problem/Rotate_Array.java
@@ -0,0 +1,19 @@
+package Array_Problem;
+
+public class Rotate_Array {
+    public static void main(String[] args) {
+
+        // Eg:  1 2 3 4 5   k=3
+        // o/p: 3 4 5 1 2
+
+        // Eg:  1 2 3 4 5   k=2
+        // o/p: 4 5 1 2 3
+
+        // Eg:  1 2 3 4 5   k=-2
+        //o/p:  3 4 5 1 2
+
+        // Eg:  1 2 3 4 5   k=6
+        //o/p:  5 1 2 3 4
+
+    }
+}
